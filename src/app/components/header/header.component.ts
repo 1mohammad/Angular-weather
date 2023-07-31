6b10c2274eac4118eee4cc2b835ab92ab5b60cf5
mohammad karimi <jaavdanm@gmail.com>
2023-07-31 13:10:13 +0330

    add l18n

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { selectIsOnline } from '@store/online-status/online-status.selectors';
 import { slideInOutAnimation } from '@animations/slide-in-out.animations';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +11,15 @@ import { slideInOutAnimation } from '@animations/slide-in-out.animations';
   animations: [slideInOutAnimation],
 })
 export class HeaderComponent {
-
-	constructor(private store: Store) {}
+	isCheckedLang = false;
+	constructor(
+		private store: Store,
+		private translateService: TranslateService
+	) {}
 
 	isOnline$ = this.store.pipe(select(selectIsOnline));
 
+	onLanguageChange():void {
+		this.translateService.use(this.isCheckedLang ? 'fa' : 'en')
+	}
 }
