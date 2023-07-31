faafa01a2649c1b8d4853afe224cee6877bb7b85
mohammad karimi <jaavdanm@gmail.com>
2023-07-31 10:56:01 +0330

    fix loading data

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -2,25 +2,33 @@ import { Component } from '@angular/core';
 import { FormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { fetchWeather } from '@store/weather/weather.actions';
+import { selectWeatherLoading } from '@store/weather/weather.selectors';
+import { take } from 'rxjs';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+	selector: 'app-search',
+	templateUrl: './search.component.html',
+	styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
 	cityForm!: UntypedFormGroup;
+	isLoading$ = this.store.select(selectWeatherLoading);
 
 	constructor(private store: Store, private fb: FormBuilder) {
 		this.cityForm = this.fb.group({
-			city: ['',Validators.required]
+			city: ['', Validators.required]
 		});
 	}
-  
+
 	onFetchWeather(): void {
-		const city = this.cityForm.get('city')?.value;
-		if (city) {
-			this.store.dispatch(fetchWeather({ city: city }));
+		if (!this.cityForm.valid) {
+			return
 		}
+		this.isLoading$.pipe(take(1)).subscribe((isLoading) => {
+			if (!isLoading) {
+				const city = this.cityForm.get('city')?.value;
+				this.store.dispatch(fetchWeather({ city: city }));
+			  }
+		});
 	}
 }
