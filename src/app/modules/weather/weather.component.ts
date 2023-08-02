c763b14a4869ae7c5884a56cb254d9b2d5b7a0ec
mohammad karimi <jaavdanm@gmail.com>
2023-08-02 13:42:14 +0330

    Fix weather card animations

--- src/app/modules/weather/weather.component.ts
+++ src/app/modules/weather/weather.component.ts
@@ -4,19 +4,20 @@ import { Store } from '@ngrx/store';
 import { selectWeatherData, selectWeatherLoading, selectWeatherError } from '@store/weather/weather.selectors';
 import { WeatherData } from '@models/weather-data.model';
 import { fadeInOutAnimation } from '@animations/fade-in-out.animations';
+import { fadeInAnimation } from '@animations/fade-in.animations';
 
 @Component({
 	selector: 'app-weather',
 	templateUrl: './weather.component.html',
 	styleUrls: ['./weather.component.scss'],
-	animations: [fadeInOutAnimation]
+	animations: [fadeInAnimation,fadeInOutAnimation]
 })
 export class WeatherComponent {
   weatherData$: Observable<WeatherData | null>;
   error$: Observable<any>;
 
   constructor(private store: Store) {
-    this.weatherData$ = this.store.select(selectWeatherData).pipe(delay(150));
-    this.error$ = this.store.select(selectWeatherError).pipe(delay(150));
+    this.weatherData$ = this.store.select(selectWeatherData);
+    this.error$ = this.store.select(selectWeatherError);
   }
 }
